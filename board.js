d1eec5df23bcd67b27c8b65f89e42826fdecbfc3
_gr_ <gilles.robit@af83.com>
2011-03-08 22:01:07 +0100

    Manage board using Objects Board, Stack and Ticket

--- board.js
+++ board.js
@@ -1,4 +1,37 @@
+
+function Board(element) {
+    var self = this;
+    this.element = element;
+    this.stacks = [];
+    this.element.find('.stack').each(function(){
+        var stack = new Stack($(this), self);
+        self.stacks.push(stack);
+    });
+}
+
+function Stack(element, board) {
+    var self = this;
+    this.board = board;
+    // our stack element
+    this.element = element;
+    // owned tickets collection
+    this.tickets = [];
+    this.element.find('article').each(function() {
+        var ticket = new Ticket($(this), self);
+        self.tickets.push(ticket);
+    });
+}
+
+function Ticket(element, stack) {
+    this.element = element;
+    this.stack = stack;
+    this.element.draggable({containment: this.stack.board.element});
+}
+
 (function($) {
-        $('.stack article').draggable();
+
+    $('.board').each(function(){
+        new Board($(this));
+    });
 
 })(jQuery);
